37ec3f36b298a584a3a169be5f21b69601735767
Alexandre Flachs <alexandre.flachs@ulb.be>
2021-11-25 16:44:19 +0100

    First commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+def main():
+    """
+    Main function of Zgoobidoo-core, is to be deleted
+    :return: None
+    """
+    print("Welcome in zgoubidoo-core")
+
+
+if __name__ == '__main__':
+    main()
